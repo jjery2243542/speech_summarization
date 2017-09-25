899d71b6ddf268a9b4d9a082d2a480624abe3221
Jerry Chou <jjery2243542@gmail.com>
2017-09-25 13:06:15 +0800

    split inference part to separate file

---
+++ inference.py
@@ -0,0 +1,35 @@
+from utils import DataGenerator
+from pointer_model import PointerModel
+
+def predict(model, iterator, output_path='result_index.txt'):
+    with open(output_path, 'w') as f_out:
+        for i, (batch_x, batch_y) in enumerate(iterator):
+            all_result = self.predict_step(batch_x)
+            for result in all_result:
+                for word_idx in result:
+                    f_out.write('{} '.format(word_idx))
+                f_out.write('\n')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-hps_path', default='./hps/cd.json')
+    parser.add_argument('-vocab_path', default='/home/jjery2243542/datasets/summary/structured/26693_50_30/vocab.pkl')
+    parser.add_argument('-model_path', default='./model/model.ckpt')
+    parser.add_argument('-dataset_path', default='/home/jjery2243542/datasets/summary/structured/26693_50_30/cd_400_100.h5')
+    parser.add_argument('-dataset_type', default='valid')
+    parser.add_argument('-output_path', default='result.txt')
+    args = parser.parse_args()
+    hps = Hps()
+    hps.load(args.hps_path)
+    hps_tuple = hps.get_tuple()
+    print(hps_tuple)
+    vocab = Vocab(args.vocab_path, args.dataset_path + '.unk.json')
+    data_generator = DataGenerator(args.dataset_path)
+    model = PointerModel(hps_tuple, vocab)
+    iterator = DataGenerator.iterator(
+        batch_size=hps_tuple.batch_size, 
+        dataset_type=args.dataset_type, 
+        infinte=False, 
+        shuffle=False
+    )
+    predict(model, iterator, args.output_path)
