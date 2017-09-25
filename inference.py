c88cd7a870f8e3022c31db04cc90e9c3bbb62fc2
Jerry Chou <jjery2243542@gmail.com>
2017-09-25 13:15:34 +0800

    add inference

--- inference.py
+++ inference.py
@@ -1,5 +1,8 @@
 from utils import DataGenerator
+from utils import Hps
+from utils import Vocab
 from pointer_model import PointerModel
+import argparse
 
 def predict(model, iterator, output_path='result_index.txt'):
     with open(output_path, 'w') as f_out:
@@ -14,7 +17,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-hps_path', default='./hps/cd.json')
     parser.add_argument('-vocab_path', default='/home/jjery2243542/datasets/summary/structured/26693_50_30/vocab.pkl')
-    parser.add_argument('-model_path', default='./model/model.ckpt')
+    parser.add_argument('-model_path', default='./model/model.ckpt-6')
     parser.add_argument('-dataset_path', default='/home/jjery2243542/datasets/summary/structured/26693_50_30/cd_400_100.h5')
     parser.add_argument('-dataset_type', default='valid')
     parser.add_argument('-output_path', default='result.txt')
@@ -26,10 +29,11 @@ if __name__ == '__main__':
     vocab = Vocab(args.vocab_path, args.dataset_path + '.unk.json')
     data_generator = DataGenerator(args.dataset_path)
     model = PointerModel(hps_tuple, vocab)
-    iterator = DataGenerator.iterator(
+    dg = DataGenerator(args.dataset_path)
+    iterator = dg.iterator(
         batch_size=hps_tuple.batch_size, 
         dataset_type=args.dataset_type, 
-        infinte=False, 
+        infinite=False, 
         shuffle=False
     )
     predict(model, iterator, args.output_path)
